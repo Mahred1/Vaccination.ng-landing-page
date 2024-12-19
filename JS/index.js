9de1c4e5a84b08e7f237986967c814f0c98e646b
Mahred1 <mredwan.kemal@gmail.com>
2024-12-19 20:37:28 +0300

    finished making the reasons reponsive for the mini desktop break point

--- JS/index.js
+++ JS/index.js
@@ -1,23 +1,31 @@
 let bannerItems= document.querySelector(".banner-items");
 let submitBanner= document.querySelector(".submit-banner");
 let banner= document.querySelector(".banner");
+let smallCards =document.querySelector(".minidesktop-cards");
+let card2 =document.querySelector(".card-2");
+let card3 =document.querySelector(".card-3");
 
 let miniDesktop = window.matchMedia("(max-width: 1178px)");
 
-function responsiveBanner(x){
-    console.log("rere");
+function responsivecomponents(x){
+;
     if (x.matches) {
-        console.log(x.matches)
+
         bannerItems.appendChild(submitBanner);
+        smallCards.appendChild(card2);
+        smallCards.appendChild(card3);
+
     }else{
         banner.appendChild(submitBanner);
+        smallCards.parentElement.appendChild(card2);
+        smallCards.parentElement.appendChild(card3);
     }
 }
 
 
 
-responsiveBanner(miniDesktop); 
+responsivecomponents(miniDesktop); 
 window.addEventListener("resize",()=>{
- responsiveBanner(miniDesktop); 
+ responsivecomponents(miniDesktop); 
     
 })
