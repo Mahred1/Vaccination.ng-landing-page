aa40fa97b0e92a6b1b36ceaf64d5dcc0dca487c4
Mahred1 <mredwan.kemal@gmail.com>
2024-12-19 22:20:58 +0300

    finished making the registration section responsive for the min desktop breakpoint. used js to restructure the dom elements for the breakpoint

--- JS/index.js
+++ JS/index.js
@@ -4,6 +4,10 @@ let banner= document.querySelector(".banner");
 let smallCards =document.querySelector(".minidesktop-cards");
 let card2 =document.querySelector(".card-2");
 let card3 =document.querySelector(".card-3");
+let regContent = document.querySelector(".registration-content");
+let regDetail = document.querySelector(".registration-details");
+let regHeader = document.querySelector(".registration-details h1");
+console.log(regHeader);
 
 let miniDesktop = window.matchMedia("(max-width: 1178px)");
 
@@ -15,10 +19,17 @@ function responsivecomponents(x){
         smallCards.appendChild(card2);
         smallCards.appendChild(card3);
 
+        firstElementCont= regContent.firstChild;
+        regContent.insertBefore(regHeader,firstElementCont);
+
     }else{
         banner.appendChild(submitBanner);
         smallCards.parentElement.appendChild(card2);
         smallCards.parentElement.appendChild(card3);
+
+        firstElementReg = regDetail.firstChild;
+
+        regDetail.insertBefore(regHeader,firstElementReg);
     }
 }
 
