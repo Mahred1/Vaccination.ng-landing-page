2a1a1f9f0580c7c98da5629bd211f3a8e043c3ca
Mahred1 <mredwan.kemal@gmail.com>
2024-12-20 16:45:43 +0300

    restructured the banner items in js for flexibility when adjusting for break points

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,11 @@
 let bannerItems= document.querySelector(".banner-items");
 let submitBanner= document.querySelector(".submit-banner");
 let banner= document.querySelector(".banner");
+let myLocation = document.querySelector(".location");
+let date = document.querySelector(".date");
+let vaccine = document.querySelector(".vaccine");
+let row1= document.querySelector(".row1");
+let row2= document.querySelector(".row2");
 let smallCards =document.querySelector(".minidesktop-cards");
 let card2 =document.querySelector(".card-2");
 let card3 =document.querySelector(".card-3");
@@ -15,15 +20,21 @@ function responsivecomponents(x){
 ;
     if (x.matches) {
 
-        bannerItems.appendChild(submitBanner);
+        row1.appendChild(myLocation);
+        row1.appendChild(date);
+        row2.appendChild(vaccine);
+        row2.appendChild(submitBanner);
         smallCards.appendChild(card2);
         smallCards.appendChild(card3);
-
+        
         firstElementCont= regContent.firstChild;
         regContent.insertBefore(regHeader,firstElementCont);
 
     }else{
         banner.appendChild(submitBanner);
+        bannerItems.appendChild(myLocation);
+        bannerItems.appendChild(date);
+        bannerItems.appendChild(vaccine);
         smallCards.parentElement.appendChild(card2);
         smallCards.parentElement.appendChild(card3);
 
